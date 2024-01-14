05bda708013404ea652c3816d1ef682ab24077e7
Denis Mironov <den.hotshot@gmail.com>
2024-01-14 15:25:26 +0100

    Update comments in bubble_sort.rb

--- algorithmic_toolbox/sorting_algorithms/bubble_sort.rb
+++ algorithmic_toolbox/sorting_algorithms/bubble_sort.rb
@@ -1,8 +1,19 @@
 # Visualisation: https://www.toptal.com/developers/sorting-algorithms
 
-# We will go through the array from left to right. If the current item is larger than the next, swap them. We do this until the array is sorted. Note that after the first iteration, the largest element will be at the end of the array, in the right place. After two iterations, the two largest elements will be in the right place, and so on. Obviously, after no more than n iterations, the array will be sorted. Thus, the asymptotic behavior in the worst and middle case is O(n2), in the best case - O(n). Note that bubble sorting works slowly on tests in which small elements are at the end (they are also called “turtles”). Such an element at each step of the algorithm will shift only one position to the left.
+# We will go through the array from left to right. If the current item is larger than the next, swap them. 
+# We do this until the array is sorted. Note that after the first iteration, the largest element will be at the end of the array, 
+# in the right place. After two iterations, the two largest elements will be in the right place, and so on. 
+# Obviously, after no more than n iterations, the array will be sorted. 
+# Thus, the asymptotic behavior in the worst and middle case is O(n2), in the best case - O(n).
+# Note that bubble sorting works slowly on tests in which small elements are at the end (they are also called “turtles”). 
+# Such an element at each step of the algorithm will shift only one position to the left.
 
-# Будем идти по массиву слева направо. Если текущий элемент больше следующего, меняем их местами. Делаем так, пока массив не будет отсортирован. Заметим, что после первой итерации самый большой элемент будет находиться в конце массива, на правильном месте. После двух итераций на правильном месте будут стоять два наибольших элемента, и так далее. Очевидно, не более чем после n итераций массив будет отсортирован. Таким образом, асимптотика в худшем и среднем случае – O(n2), в лучшем случае – O(n). Заметим, что сортировка пузырьком работает медленно на тестах, в которых маленькие элементы стоят в конце (их еще называют «черепахами»). Такой элемент на каждом шаге алгоритма будет сдвигаться всего на одну позицию влево.
+# Будем идти по массиву слева направо. Если текущий элемент больше следующего, меняем их местами. 
+# Делаем так, пока массив не будет отсортирован. Заметим, что после первой итерации самый большой элемент будет находиться в конце массива, 
+# на правильном месте. После двух итераций на правильном месте будут стоять два наибольших элемента, и так далее. 
+# Очевидно, не более чем после n итераций массив будет отсортирован. Таким образом, асимптотика в худшем и среднем случае – O(n2), в лучшем случае – O(n). 
+# Заметим, что сортировка пузырьком работает медленно на тестах, в которых маленькие элементы стоят в конце (их еще называют «черепахами»). 
+# Такой элемент на каждом шаге алгоритма будет сдвигаться всего на одну позицию влево.
 
 puts 'Please, enter array size'
 
